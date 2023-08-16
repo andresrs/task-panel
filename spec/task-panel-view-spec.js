5ee921fac0bd4a7af042c478fb9adbd570aadf7a
Andres Rosado <andres.rosado@gmail.com>
2023-08-16 14:41:40 -0400

    Package generated

---
+++ spec/task-panel-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TaskPanelView from '../lib/task-panel-view';
+
+describe('TaskPanelView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
